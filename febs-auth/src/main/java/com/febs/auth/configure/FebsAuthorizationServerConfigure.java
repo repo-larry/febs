a53bdfd8e61f466b18b70800bec595bee5ec8ced
songyx <2250794569@qq.com>
2022-10-16 19:52:21 +0800

    token生成策略优化

--- febs-auth/src/main/java/com/febs/auth/configure/FebsAuthorizationServerConfigure.java
+++ febs-auth/src/main/java/com/febs/auth/configure/FebsAuthorizationServerConfigure.java
@@ -22,6 +22,8 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
+import java.util.UUID;
+
 /**
  * @description:
  * @date: 2022/9/18
@@ -66,7 +68,11 @@ public class FebsAuthorizationServerConfigure extends AuthorizationServerConfigu
 
     @Bean
     public TokenStore tokenStore() {
-        return new RedisTokenStore(redisConnectionFactory);
+        RedisTokenStore redisTokenStore = new RedisTokenStore(redisConnectionFactory);
+        // 解决每次生成的 token都一样的问题
+        redisTokenStore.setAuthenticationKeyGenerator(oAuth2Authentication -> UUID.randomUUID().toString());
+        return redisTokenStore;
+//        return new RedisTokenStore(redisConnectionFactory);
     }
 
     @Primary
